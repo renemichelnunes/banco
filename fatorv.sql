70a84d94d0f4c91e53eee28b954381fa304c72b8
renemichelnunes <rene.michel@gmail.com>
2024-04-03 21:21:03 -0300

    banco

--- fatorv.sql
+++ fatorv.sql
@@ -16,28 +16,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: fatorv; Type: DATABASE; Schema: -; Owner: fatorv
---
-
-CREATE DATABASE fatorv WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'pt_BR.UTF-8';
-
-
-ALTER DATABASE fatorv OWNER TO fatorv;
-
-\connect fatorv
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
